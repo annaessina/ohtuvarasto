64aefbff2d89f59c08a8c4559a4742ddc0f3ccf9
Anna Essina <anna.essina@helsinki.fi>
2023-11-04 01:53:02 +0200

    testit rikki

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -2,11 +2,11 @@ import unittest
 from varasto import Varasto
 
 
-class TestVarasto(unittest.TestCase):
-    def setUp(self):
+#class TestVarasto(unittest.TestCase):
+   # def setUp(self):
         self.varasto = Varasto(10, alku_saldo=0)
 
-    def test_konstruktori_luo_tyhjan_varaston(self):
+   # def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
